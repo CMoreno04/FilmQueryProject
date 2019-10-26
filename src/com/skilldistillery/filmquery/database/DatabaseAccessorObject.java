4afa8916124752879410af32d6bf640652390206
CMoreno04 <cesar.jos.em26@icloud.com>
2019-10-25 20:13:06 -0600

    Updated

--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -1,27 +1,65 @@
 package com.skilldistillery.filmquery.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class DatabaseAccessorObject implements DatabaseAccessor {
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
 
-  @Override
-  public Film findFilmById(int filmId) {
-    return null;
-  }
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public Film findFilmById(int filmId) {
 
-@Override
-public Actor findActorById(int actorId) {
-	// TODO Auto-generated method stub
-	return null;
-}
+		return null;
+	}
 
-@Override
-public List<Actor> findActorsByFilmId(int filmId) {
-	// TODO Auto-generated method stub
-	return null;
-}
+	@Override
+	public Actor findActorById(int actorId) {
+		String user = "student";
+		String pass = "student";
+		String sqlTxt = "INSERT SQL COMMAND";
+
+		Actor actor = null;
+
+		try (Connection conn = DriverManager.getConnection(URL, user, pass);
+				PreparedStatement stmnt = conn.prepareStatement(sqlTxt);
+				ResultSet rs = stmnt.executeQuery();) {
+
+			while (rs.next()) {
+				actor.setFilms();
+			}
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			return actor;
+
+		}
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		List<Actor> films = new ArrayList<>();
+
+		return films;
+	}
 
 }
